82c3d06fac1a859b89c0b97af2799db99507f2b7
edward <fanofparadox@gmail.com>
2022-05-29 23:31:47 +0300

    csv parser added

---
+++ Parser.php
@@ -0,0 +1,31 @@
+<?php
+
+class Parser {
+
+  public $db;
+
+  function __construct($filename, $dbname = 'wapidb', $host = '127.0.0.1', $user = 'edward', $password = '555777') {
+    $this->db = new PDO("mysql:dbname=" . $dbname . ";host=" . $host, $user, $password);
+    $rows = $this->prepare_csv($filename);
+    $this->dump_data($rows, $filename);
+  }
+
+  public function prepare_csv($filename) {
+    $rows = file("./data/" . $filename . ".csv");
+    // Remove row with headers
+    array_splice($rows, 0, 1);
+    // Convert row-strings to array-strings
+    foreach($rows as $key => $row) {
+      $rows[$key] = explode(',', $row);
+    }
+    return $rows;
+  }
+
+  public function dump_data($rows, $filename) {
+    foreach($rows as $row) {
+      $place_holders = implode(',', array_fill(0, count($row), '?'));
+      $sth = $this->db->prepare("INSERT INTO $filename VALUES($place_holders);");
+      $sth->execute($row);
+    }
+  }
+}
